9d33a39ea5f7dd3db0204aac6c8264f6f00ad3c9
Anzyll <anzil2003m@gmail.com>
2026-03-29 20:51:46 +0530

    chore: setup Spring Boot project and base structure

---
+++ src/main/java/com/flashsale/ordersystem/FlashSaleOrderSystemApplication.java
@@ -0,0 +1,12 @@
+package com.flashsale.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSaleOrderSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSaleOrderSystemApplication.class, args);
+    }
+
+}
