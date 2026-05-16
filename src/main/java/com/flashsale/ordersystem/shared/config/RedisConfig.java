940485ebb9b0a5b4336824e672d0ad468bb835c1
Anzyll <anzil2003m@gmail.com>
2026-05-17 01:34:07 +0530

    feat(deploy): Move PostgreSQL to AWS RDS

--- src/main/java/com/flashsale/ordersystem/shared/config/RedisConfig.java
+++ src/main/java/com/flashsale/ordersystem/shared/config/RedisConfig.java
@@ -1,15 +1,18 @@
 package com.flashsale.ordersystem.shared.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import io.github.bucket4j.distributed.proxy.ProxyManager;
 import io.github.bucket4j.redis.lettuce.cas.LettuceBasedProxyManager;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.codec.ByteArrayCodec;
-import io.lettuce.core.codec.RedisCodec;
 import io.lettuce.core.codec.StringCodec;
+import io.lettuce.core.codec.RedisCodec;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class RedisConfig {
@@ -21,6 +24,21 @@ public class RedisConfig {
     private int redisPort;
 
     @Bean
+    public DefaultRedisScript<Long> purchaseScript() {
+
+        DefaultRedisScript<Long> script =
+                new DefaultRedisScript<>();
+
+        script.setLocation(
+                new ClassPathResource("scripts/stock_purchase.lua")
+        );
+
+        script.setResultType(Long.class);
+
+        return script;
+    }
+
+    @Bean
     public ProxyManager<String> proxyManager() {
 
         RedisClient redisClient =
@@ -29,10 +47,12 @@ public class RedisConfig {
                 );
 
         StatefulRedisConnection<String, byte[]> connection =
-                redisClient.connect(RedisCodec.of(
-                        StringCodec.UTF8,
-                        ByteArrayCodec.INSTANCE
-                ));
+                redisClient.connect(
+                        RedisCodec.of(
+                                StringCodec.UTF8,
+                                ByteArrayCodec.INSTANCE
+                        )
+                );
 
         return LettuceBasedProxyManager
                 .builderFor(connection)
